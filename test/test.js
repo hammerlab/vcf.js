a7d0e0e50e6834b8e4f4032fd7f01e073b5a9685
Isaac Hodes <isaachodes@gmail.com>
2014-10-19 23:07:39 -0400

    Add Mocha tests
    A few sanity tests to make sure things are working.

---
+++ test/test.js
@@ -0,0 +1,40 @@
+var assert = require("assert"),
+    fs = require("fs"),
+    vcf = require("../vcf.js");
+
+
+describe('VCF.js', function(){
+  describe('#parser', function(){
+    it('should parse, without error, a VCF', function(){
+      // TODO(ihodes): speed up VCF.js and parse a Mutect and other callers' VCFs.
+      var failed = false;
+      try {
+        vcf.parser()(fs.readFileSync('test/data/snv.vcf', {encoding: 'utf8'}));
+      } catch (e) {
+        failed = true;
+      }
+      assert(!failed);
+    });
+    it('should parse all 10 rows of a VCF', function(){
+      var failed = false,
+          data = [];
+      try {
+        data = vcf.parser()(String(fs.readFileSync('test/data/snv.vcf')));
+      } catch (e) {
+        failed = true;
+      }
+      assert(!failed && data.records.length == 10);
+    });
+    it('should throw on parsing a VCF of unrecognized version', function(){
+      var failed = false;
+      try {
+        vcf.parser()(String(fs.readFileSync('test/data/bad-version.vcf')));
+      } catch (e) {
+        if (e.message.indexOf("version") > -1) {
+          failed = true;
+        }
+      }
+      assert(failed);
+    });
+  });
+});
